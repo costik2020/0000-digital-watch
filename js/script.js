743005f9f2c844ec38d9f296245f5ce6783f5dcf
costikluciu2020 <costikluciu2020@gmail.com>
2022-07-04 14:49:22 +0100

    Initial start of the small watch

---
+++ js/script.js
@@ -0,0 +1,37 @@
+//console.log("linked!");
+
+
+
+//- Grab an element from that page
+//- Display the clock on that element that I grabed
+//	- See if I have a javascript function that can give me a time or date.
+//	- call that function each second
+//	- update the digital clock
+
+
+//- Grab an element from that page
+let watch = document.querySelector(".watch");
+
+//- Display the clock on that element that I grabed
+watch.textContent = "hh:mm:ss";
+//console.log("watch=", watch);
+
+//	- See if I have a javascript function that can give me a time or date.
+const today = new Date();
+console.log("today=", today);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
